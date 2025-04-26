62f6a76729f2090c18970bfa0e1d7a4137ad5210
Bruno Batista <bjnb.projetos@gmail.com>
2025-04-25 23:35:51 -0300

    feat: create loading and login screen

---
+++ app/src/main/java/com/brunojbatista/virtualcamapp/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.brunojbatista.virtualcamapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); // usa o layout que você já criou
+    }
+}
